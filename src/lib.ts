6257f42573d27d647d2fff5cbd8b567a7f9a1f5b
Oscar Ruiz <oscaralexruiz@gmail.com>
2026-09-14 00:20:51 -0500

    feat(api): programmatic API surface

---
+++ src/lib.ts
@@ -0,0 +1,5 @@
+// Programmatic API for codeforge (also used by the test/verification harness).
+export * from '@exercises';
+export * from '@utils/config';
+export * from '@utils/generate';
+export * from '@utils/runTests';
